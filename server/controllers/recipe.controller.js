6d6404cd889ebd9e2b5be5f0ce57ecb8c89e00b9
augustincga <gabriel.augustin1995@yahoo.com>
2018-03-28 14:25:36 +0200

    (Server) Filter recipes based on filters and ingredients

--- server/controllers/recipe.controller.js
+++ server/controllers/recipe.controller.js
@@ -5,7 +5,7 @@ exports.findRecipeByIngredients = function(req, res) {
 		return res.status(500).send({message: req.body})
 	}
 
-	let ingredients = req.body.ingredientsName;
+	let ingredients = req.body.ingredients;
 
 	Recipe.find({'ingredients.name':  {$all:ingredients}}, function(err, recipes){
 		if(err) {
@@ -14,6 +14,30 @@ exports.findRecipeByIngredients = function(req, res) {
 			res.status(200).send(recipes);
 		} else {
 			res.status(404).send("Could not find recipes by requested ingredients");
+		}
+	});
+};
+
+exports.filterRecipes = function(req, res) {
+	if(!req.body) {
+		res.status(500).send({message: req.body});
+	};
+
+	filter = {};
+	Object.assign(filter, req.body.filters ? {categories: req.body.filters } : null,
+						  req.body.ingredients ? {ingredients: req.body.ingredients } : null);
+
+	searchQuery = {};
+	Object.assign(searchQuery, filter.categories ? {'categories': {$all: filter.categories} } : null,
+						   	   filter.ingredients? {'ingredients.name': {$all: filter.ingredients} } : null);
+
+	Recipe.find(searchQuery, function(err, recipes){
+		if(err) {
+			res.status(500).send({message: "Some error occurred while searching for the Recipes."});
+		} else if(recipes && recipes !== null) {
+			res.status(200).send(recipes);
+		} else {
+			res.status(404).send("Could not find recipes by selected filters.");
 		}
 	});
 };
--- server/routes/recipe.routes.js
+++ server/routes/recipe.routes.js
@@ -2,4 +2,5 @@ var recipeController = require('../controllers/recipe.controller')
 
 module.exports = function(app){
 	app.post('/api/recipes-by-ingredients', recipeController.findRecipeByIngredients);
+	app.post('/api/filter-recipes', recipeController.filterRecipes)
 }
