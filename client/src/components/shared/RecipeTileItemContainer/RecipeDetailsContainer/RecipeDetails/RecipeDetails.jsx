90d8da7a13c93f4ce5577da37432ee38eb215067
augustincga <gabriel.augustin1995@yahoo.com>
2018-04-24 12:25:05 +0200

    (Client) Add export recipe as pdf

--- client/src/components/shared/RecipeTileItemContainer/RecipeDetailsContainer/RecipeDetails/RecipeDetails.jsx
+++ client/src/components/shared/RecipeTileItemContainer/RecipeDetailsContainer/RecipeDetails/RecipeDetails.jsx
@@ -3,6 +3,11 @@ import Dialog from 'material-ui/Dialog'
 import Divider from 'material-ui/Divider'
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+
 
 import './RecipeDetails.css'
 import {cookies} from '../../../constants'
@@ -15,8 +20,9 @@ class RecipeDetails extends Component {
 		this.state = {
 			recipeDetailsData: this.props.recipeDetailsData
 		}
-		this._getReviewDetails = this._getReviewDetails.bind(this),
-		this._onAddReview = this._onAddReview.bind(this)
+		this._getReviewDetails = this._getReviewDetails.bind(this);
+		this._onAddReview = this._onAddReview.bind(this);
+		this._exportRecipeAsPdf = this._exportRecipeAsPdf.bind(this);
 	}
 
 	render() {
@@ -27,69 +33,78 @@ class RecipeDetails extends Component {
 				onRequestClose={this.props.onRecipeDetailsModalClose}
 				contentClassName='recipe-details__modal-wrapper'
 			>
-				<div className="recipe-details__title">
-					<h1>{this.state.recipeDetailsData.title}</h1>
-				</div>
-				<div className="recipe-details__header-wrapper">
-					<div className="recipe-picture">
-						<img src={this.state.recipeDetailsData.smallImage}/>
+				<div className="recipe-details__header-buttons">
+					<div className="header-buttons__export-pdf">
+						<IconButton iconClassName="fa fa-save" onClick={this._exportRecipeAsPdf}/>
 					</div>
-					<div className="recipe-data">
-						<div className="recipe-information-wrapper">
-							<div className="recipe-cooking-time">
-								<i className=""> {this.state.recipeDetailsData.cookingTime}</i>
+				</div>
+				
+				<div className="recipe-details__content" id="sectionToPrint">
+					<div className="recipe-details__title">
+						<h1>{this.state.recipeDetailsData.title}</h1>
+					</div>
+
+					<div className="recipe-details__header-wrapper">
+						<div className="recipe-picture">
+							<img src={this.state.recipeDetailsData.smallImage}/>
+						</div>
+						<div className="recipe-data">
+							<div className="recipe-information-wrapper">
+								<div className="recipe-cooking-time">
+									<i className=""> {this.state.recipeDetailsData.cookingTime}</i>
+								</div>
+								<div className="recipe-servings">
+									<i className=""> {this.state.recipeDetailsData.servings}</i>
+								</div>
 							</div>
-							<div className="recipe-servings">
-								<i className=""> {this.state.recipeDetailsData.servings}</i>
+							<div className="recipe-nutrients-wrapper">
+								<div className="recipe-calories">
+									<i className=""> Calories: {this.state.recipeDetailsData.nutrients.calories}</i>
+								</div>
+								<div className="recipe-carbs">
+									<i className=""> Carbs: {this.state.recipeDetailsData.nutrients.carbohydrates}</i>
+								</div>
+								<div className="recipe-protein">
+									<i className=""> Protein: {this.state.recipeDetailsData.nutrients.protein}</i>
+								</div>
+								<div className="recipe-fat">
+									<i className=""> Fat: {this.state.recipeDetailsData.nutrients.fat}</i>
+								</div>
 							</div>
 						</div>
-						<div className="recipe-nutrients-wrapper">
-							<div className="recipe-calories">
-								<i className=""> Calories: {this.state.recipeDetailsData.nutrients.calories}</i>
-							</div>
-							<div className="recipe-carbs">
-								<i className=""> Carbs: {this.state.recipeDetailsData.nutrients.carbohydrates}</i>
-							</div>
-							<div className="recipe-protein">
-								<i className=""> Protein: {this.state.recipeDetailsData.nutrients.protein}</i>
-							</div>
-							<div className="recipe-fat">
-								<i className=""> Fat: {this.state.recipeDetailsData.nutrients.fat}</i>
-							</div>
+					</div>
+					<Divider/>
+					<div className="recipe-details__section-wrapper ">
+						<div className="recipe-details__section-title">
+							<h1>Ingredients</h1>
 						</div>
+						{this.state.recipeDetailsData.ingredients.map((ingredient, index) => {
+							return (
+								<div className="recipe-details__section-item" key={ingredient.name}>
+									<i className="fa fa-caret-right"> 
+										<span className="ingredient-item-amount">{ingredient.amount}</span>
+										<span className="ingredient-item-unit">{ingredient.unit}</span>
+										<span className="ingredient-item-name">{ingredient.name}</span>
+									</i>
+								</div>
+							)
+						})}
 					</div>
-				</div>
-				<Divider/>
-				<div className="recipe-details__section-wrapper">
-					<div className="recipe-details__section-title">
-						<h1>Ingredients</h1>
+					<Divider/>
+					<div className="recipe-details__section-wrapper">
+						<div className="recipe-details__section-title">
+							<h1>Steps</h1>
+						</div>
+						{this.state.recipeDetailsData.cookingSteps.map((step, index) => {
+							return (
+								<div className="recipe-details__section-item" key={step}>
+									<i className="fa fa-caret-right"> 
+										<span className="step-description">{step}</span>
+									</i>
+								</div>
+							)
+						})}
 					</div>
-					{this.state.recipeDetailsData.ingredients.map((ingredient, index) => {
-						return (
-							<div className="recipe-details__section-item" key={ingredient.name}>
-								<i className="fa fa-caret-right"> 
-									<span className="ingredient-item-amount">{ingredient.amount}</span>
-									<span className="ingredient-item-unit">{ingredient.unit}</span>
-									<span className="ingredient-item-name">{ingredient.name}</span>
-								</i>
-							</div>
-						)
-					})}
-				</div>
-				<Divider/>
-				<div className="recipe-details__section-wrapper">
-					<div className="recipe-details__section-title">
-						<h1>Steps</h1>
-					</div>
-					{this.state.recipeDetailsData.cookingSteps.map((step, index) => {
-						return (
-							<div className="recipe-details__section-item" key={step}>
-								<i className="fa fa-caret-right"> 
-									<span className="step-description">{step}</span>
-								</i>
-							</div>
-						)
-					})}
 				</div>
 				<Divider/>
 				<div className="recipe-details__section-wrapper">
@@ -186,6 +201,21 @@ class RecipeDetails extends Component {
 				})
 			}
 		}.bind(this))
+	}
+
+	_exportRecipeAsPdf() {
+		const sectionToPrint = document.getElementById('sectionToPrint');
+
+
+		html2canvas(sectionToPrint)
+			.then((canvas) => {
+				const imgData = canvas.toDataURL('image/png');
+				const pdf = new jsPDF();
+				pdf.addImage(imgData, 'JPEG', 10, 10, 180, 150);
+				pdf.save("recipe.pdf");
+			});
+	  
+
 	}
 }
 
