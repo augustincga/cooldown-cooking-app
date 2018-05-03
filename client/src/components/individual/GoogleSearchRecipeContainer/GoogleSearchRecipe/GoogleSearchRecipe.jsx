17769754910e7e8da68ce72a4544067248942445
augustincga <gabriel.augustin1995@yahoo.com>
2018-05-03 15:58:44 +0200

    (Client) - Add google searches list. Refactor Google Search Component

--- client/src/components/individual/GoogleSearchRecipeContainer/GoogleSearchRecipe/GoogleSearchRecipe.jsx
+++ client/src/components/individual/GoogleSearchRecipeContainer/GoogleSearchRecipe/GoogleSearchRecipe.jsx
@@ -1,72 +1,51 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 
-
 import './GoogleSearchRecipe.css'
-import {CLOUDINARY_UPLOAD_PRESET, CLOUDINARY_UPLOAD_URL} from '../../../shared/constants'
+import LoadingIndicator from '../../../shared/LoadingIndicator/LoadingIndicator'
 
 class GoogleSearchRecipe extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			uploadedFileCloudinaryUrl: '',
-			uploadedFile: ''
+
 		}
-		this._onImageDrop = this._onImageDrop.bind(this);
-		this._handleImageUpload = this._handleImageUpload.bind(this);
 	}
 
 	render() {
 		return (
+			
 			<div className="google-search-recipes__wrapper">
-				<Dropzone
-					multiple={false}
-					accept="image/*"
-					onDrop={this._onImageDrop}>
-					<p>Drop an image or click to select a file to upload.</p>
-				</Dropzone>
+				<LoadingIndicator isActive = {this.props.isLoadingActive}/>
 
-				<div>
-					{this.state.uploadedFileCloudinaryUrl === '' ? null :
-						<div>
-							<p>{this.state.uploadedFile.name}</p>
-							<img src={this.state.uploadedFileCloudinaryUrl} />
-						</div>}
+				<div className="google-search-recipes__dropzone">
+					<Dropzone
+						multiple={false}
+						accept="image/*"
+						onDrop={this.props.onImageDrop}>
+						<p>Drop an image or click to select a file to upload.</p>
+					</Dropzone>
 				</div>
+
+				<div className="google-search-recipes__list-container">
+					<div>
+						<h1>Here are your latest google searches...</h1>
+					</div>
+
+					<div>
+						{this.props.googleSearchedRecipes.map((search) => {
+							return(
+								<div className="google-search-recipes__list-item" key={search.urlResult}>
+									<a href={search.urlResult} target="_blank"><img src={search.uploadedImgUrl}/></a>
+								</div>
+							)
+						})}
+					</div>
+				</div>
+			
 			</div>
 		);
 	}
-
-	_onImageDrop(files) {
-		this.setState({
-			uploadedFile: files[0]
-		})
-		this._handleImageUpload(files[0]);
-	}
-
-	_handleImageUpload(file) {
-
-		let fd = new FormData();
-		fd.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
-		fd.append('file', file);
-
-		fetch(CLOUDINARY_UPLOAD_URL, {
-			method: 'post',
-			body: fd
-		})
-		.then()
-		.then()
-		.then((response) => {
-			response.json().then((image) => {
-				this.setState({
-					uploadedFileCloudinaryUrl: image.secure_url
-				  });
-				  
-				  window.open(`http://images.google.com/searchbyimage?image_url=${image.secure_url}`, '_blank');
-			})
-		  });
-	
-	  }
 }
 
 export default GoogleSearchRecipe;
--- client/src/components/individual/GoogleSearchRecipeContainer/GoogleSearchRecipeContainer.jsx
+++ client/src/components/individual/GoogleSearchRecipeContainer/GoogleSearchRecipeContainer.jsx
@@ -1,21 +1,98 @@
 import React, { Component } from 'react';
 
 import GoogleSearchRecipe from './GoogleSearchRecipe/GoogleSearchRecipe'
+import {CLOUDINARY_UPLOAD_PRESET, CLOUDINARY_UPLOAD_URL, cookies, successNotification, errorNotification} from '../../shared/constants'
+
 
 class GoogleSearchRecipeContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			
+			uploadedFileCloudinaryUrl: '',
+			uploadedFile: '',
+			googleSearchedRecipes: cookies.get('user').googleSearchedRecipes,
+			isLoadingActive: false		
 		}
+		this._onImageDrop = this._onImageDrop.bind(this);
+		this._handleImageUpload = this._handleImageUpload.bind(this);
+		this._saveGoogleSearch = this._saveGoogleSearch.bind(this);
 	}
 
 	render() {
 		return (
 			<div>
-				<GoogleSearchRecipe/>
+				<GoogleSearchRecipe
+					onImageDrop = {this._onImageDrop}
+					googleSearchedRecipes = {this.state.googleSearchedRecipes} 
+					isLoadingActive = {this.state.isLoadingActive}
+				/>
 			</div>
 		);
+	}
+
+	_onImageDrop(files) {
+		this.setState({
+			uploadedFile: files[0],
+			isLoadingActive: true
+		})
+		this._handleImageUpload(files[0]);
+	}
+
+	_handleImageUpload(file) {
+
+		let fd = new FormData();
+		fd.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+		fd.append('file', file);
+
+		fetch(CLOUDINARY_UPLOAD_URL, {
+			method: 'post',
+			body: fd
+		})
+		.then()
+		.then()
+		.then((response) => {
+			response.json().then((image) => {
+				let googleUrl = `http://images.google.com/searchbyimage?image_url=${image.secure_url}`
+
+				window.open(googleUrl, '_blank');
+				this._saveGoogleSearch(image.secure_url, googleUrl);
+			})
+		  });
+		  this._onImageDrop = this._onImageDrop.bind(this);
+		  this._handleImageUpload = this._handleImageUpload.bind(this);
+		  this._saveGoogleSearch = this._saveGoogleSearch.bind(this);
+	  }
+
+	_saveGoogleSearch(imageUrl, urlResult) {
+		let data = {
+			uploadedImgUrl: imageUrl,
+			urlResult: urlResult,
+			userId: cookies.get('user')._id
+		}
+
+		fetch('http://localhost:3001/api/user/saveGoogleSearch', {
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'post',
+			body: JSON.stringify(data)
+		}).then((response) => {
+			if(response.status === 200) {
+				response.json().then((user) => {
+					cookies.set('user', user);
+					this.setState({
+						googleSearchedRecipes: user.googleSearchedRecipes,
+						isLoadingActive: false
+					})
+					successNotification('Google Search has been saved.');
+				})
+			} else {
+				response.json().then((err) => {
+					errorNotification(err);
+				})
+			}
+		});
 	}
 }
 
